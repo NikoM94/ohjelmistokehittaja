505e6f238d5c68cd7f968e9dc3e0133f5272362e
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2026-01-28 22:06:12 +0200

    Taulukot

--- LumoTehtavat/Program.cs
+++ LumoTehtavat/Program.cs
@@ -6,10 +6,144 @@ class Program
 {
     static void Main(string[] args)
     {
-        // PrintSorted();
         // PrintToUpper();
+        // PrintShoppingList();
+        // PrintFiveNames();
+        // PrintArrLength();
+        // PrintAddName();
+        // PrintCombineArrays();
+        // Taulukot.PrintEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        // PrintSortAscending();
         // PrintReversed();
-        PrintConvertedToString();
+        // PrintConvertedToString();
+        // PrintRemoveAt();
+        // PrintSumArray();
+        // PrintIndexOf();
+        // PrintSliceFromTail();
+        // PrintSwapMiddle();
+        // PrintAverageArray();
+        // PrintMultiplyElementsNTimes();
+        // PrintUnique();
+        // PrintRemoveSmallerThan();
+        PrintSwapHeadAndTail();
+    }
+
+    private static void PrintSwapHeadAndTail()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        Console.WriteLine($"Before swapping first and last element: {ToString(ints)} ");
+        Taulukot.SwapHeadAndTail(ints);
+        Console.WriteLine($"First and last element swapped: {ToString(ints)}");
+    }
+
+    private static void PrintRemoveSmallerThan()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        int bound = 4;
+        int[] removed = Taulukot.RemoveSmallerThan(ints, bound);
+        Console.WriteLine($"Array with all numbers smaller than {bound} removed: {ToString(removed)}");
+    }
+
+    private static void PrintUnique()
+    {
+        int[] ints = [1, 2, 2, 3, 4, 4, 5];
+        Console.WriteLine($"Before removing duplicates: {ToString(ints)}");
+        int[] unique = Taulukot.Unique(ints);
+        Console.WriteLine($"After removing duplicates: {ToString(unique)}");
+
+    }
+
+    private static void PrintMultiplyElementsNTimes()
+    {
+        int[] ints = [1, 2, 3];
+        int times = 3;
+        Console.WriteLine($"Before multiplying {times} times: {ToString(ints)}");
+        int[] multiplied = Taulukot.MultiplyElementsNTimes(ints, 3);
+        Console.WriteLine($"After multiplying {times} times: {ToString(multiplied)}");
+    }
+
+    private static void PrintAverageArray()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        int avg = Taulukot.Average(ints);
+        Console.WriteLine($"Elements of array {ToString(ints)} averages to {avg}");
+    }
+
+    private static void PrintSwapMiddle()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        int newValue = 6;
+        Console.WriteLine($"Before swapping the middle value to {newValue}: {ToString(ints)}");
+        Taulukot.SwapMiddle(ints, newValue);
+        Console.WriteLine($"After swap: {ToString(ints)}");
+    }
+
+    private static void PrintSliceFromTail()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        int amount = 2;
+        int[] sliced = Taulukot.SliceFromTail(ints, amount);
+        Console.WriteLine($"{amount} elements from the end of {ToString(ints)}: {ToString(sliced)}");
+    }
+
+    private static void PrintIndexOf()
+    {
+        int[] ints = [1, 2, 3, 4, 5];
+        int value = 2;
+        int indexOf = Taulukot.IndexOf(ints, value);
+        if (indexOf == -1)
+        {
+            Console.WriteLine($"Value {value} does not exist in array {ToString(ints)}");
+        }
+        else
+        {
+            Console.WriteLine($"Value {value} found at index: {indexOf} of {ToString(ints)}");
+        }
+    }
+
+    private static void PrintSumArray()
+    {
+        int[] arr = [1, 2, 3, 4, 5];
+        Console.WriteLine($"The elements in the array {ToString(arr)} sums to {Taulukot.SumArray(arr)}");
+    }
+
+    private static void PrintRemoveAt()
+    {
+        int[] result = Taulukot.RemoveAt([1, 2, 3, 4, 5], 2);
+        Console.WriteLine(ToString(result));
+    }
+
+    private static void PrintShoppingList()
+    {
+        var shoppingList = Taulukot.CreateShoppingList();
+        Console.WriteLine(ToString(shoppingList));
+    }
+
+    private static void PrintFiveNames()
+    {
+        string[] names = Taulukot.CreateFiveNames("Aino", "Aila", "Aleksi", "Aapo", "Akseli");
+        Console.WriteLine(ToString(names));
+    }
+
+    private static void PrintArrLength()
+    {
+        int[] example = [1, 2, 3, 4, 5];
+        Console.WriteLine($"Array {ToString(example)} is of length: {Taulukot.ArrLength(example)}");
+    }
+
+    private static void PrintAddName()
+    {
+        string[] names = ["Matti", "Anna", "Eeva"];
+        Taulukot.AddName(names, "Aino");
+        Console.WriteLine(ToString(names));
+    }
+
+    private static void PrintCombineArrays()
+    {
+        int[] a = [1, 2, 3];
+        int[] b = [4, 5, 6];
+        int[] combined = Taulukot.CombineArrays(a, b);
+        Console.WriteLine(ToString(combined));
     }
 
     private static void PrintConvertedToString()
@@ -22,38 +156,44 @@ class Program
     {
         var toPrint = new int[] { 5, 4, 3, 2, 1 };
         var reversed = Taulukot.Reverse(toPrint);
-        var builder = new StringBuilder();
-        builder.Append('[');
-        foreach (var item in reversed)
-        {
-            builder.Append($"{item}, ");
-        }
-        Console.WriteLine($"{builder.ToString().Trim(' ').Trim(',')}]");
+        Console.WriteLine(ToString(reversed));
     }
 
     private static void PrintToUpper()
     {
-        var toPrint = new string[] { "Matti", "Anna", "Eeva" };
-        Taulukot.ToUpper(toPrint);
+        var toUpper = new string[] { "Matti", "Anna", "Eeva" };
+        Taulukot.ToUpper(toUpper);
+        Console.WriteLine(ToString(toUpper));
+    }
+
+    private static void PrintSortAscending()
+    {
+        var toSort = new int[] { 5, 2, 9, 1, 5, 8, 3 };
+        Taulukot.SortAscending(toSort);
+        Console.WriteLine(ToString(toSort));
+    }
+
+    private static string ToString(int[] toPrint)
+    {
         var builder = new StringBuilder();
         builder.Append('[');
         foreach (var item in toPrint)
         {
             builder.Append($"{item}, ");
         }
-        Console.WriteLine($"{builder.ToString().Trim(' ').Trim(',')}]");
+        return $"{builder.ToString().Trim(' ').Trim(',')}]";
+
     }
 
-    private static void PrintSorted()
+
+    private static string ToString(string[] toPrint)
     {
-        var toSort = new int[] { 5, 2, 9, 1, 5, 8, 3 };
-        Taulukot.SortAscending(toSort);
         var builder = new StringBuilder();
         builder.Append('[');
-        foreach (var item in toSort)
+        foreach (var item in toPrint)
         {
             builder.Append($"{item}, ");
         }
-        Console.WriteLine($"{builder.ToString().Trim(' ').Trim(',')}]");
+        return $"{builder.ToString().Trim(' ').Trim(',')}]";
     }
 }
--- LumoTehtavat/Taulukot/Taulukot.cs
+++ LumoTehtavat/Taulukot/Taulukot.cs
@@ -2,25 +2,60 @@ using System.Text;
 
 public static class Taulukot
 {
-    public static int[] Reverse(int[] arr)
+    //T01
+    public static string[] CreateShoppingList()
     {
-        var reversed = new int[arr.Length];
-        for (int i = arr.Length - 1, j = 0; i >= 0; i--, j++)
-        {
-            reversed[j] = arr[i];
-        }
-        return reversed;
+        return ["omena", "leipä", "piimä"];
     }
 
-    //T09
-    public static string ConvertToString(string[] arr)
+    //T02
+    public static string[] CreateFiveNames(string a, string b, string c, string d, string e)
     {
-        var builder = new StringBuilder();
-        foreach (var s in arr)
+        return [a, b, c, d, e];
+    }
+
+    //T03
+    public static int ArrLength(int[] arr)
+    {
+        return arr.Length;
+    }
+
+    //T04
+    public static string[] AddName(string[] arr, string nameToAdd)
+    {
+        var newArr = new string[arr.Length + 1];
+
+        for (int i = 0; i < arr.Length; i++)
         {
-            builder.Append(s).Append(", ");
+            newArr[i] = arr[i];
         }
-        return builder.ToString().Trim(' ').Trim(',');
+
+        newArr[newArr.Length - 1] = nameToAdd;
+        return newArr;
+    }
+
+    //T05
+    public static int[] CombineArrays(int[] a, int[] b)
+    {
+        int[] newArr = new int[a.Length + b.Length];
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            newArr[i] = a[i];
+        }
+
+        for (int i = b.Length, j = 0; i < a.Length + b.Length; i++, j++)
+        {
+            newArr[i] = b[j];
+        }
+
+        return newArr;
+    }
+
+    //T06
+    public static int[] EvenNumbers(int[] arr)
+    {
+        return [.. arr.Where(x => x % 2 == 0)];
     }
 
     // T07
@@ -39,6 +74,117 @@ public static class Taulukot
                 }
             }
         }
+    }
+
+    //T08
+    public static int[] Reverse(int[] arr)
+    {
+        var reversed = new int[arr.Length];
+        for (int i = arr.Length - 1, j = 0; i >= 0; i--, j++)
+        {
+            reversed[j] = arr[i];
+        }
+        return reversed;
+    }
+
+
+    //T09
+    public static string ConvertToString(string[] arr)
+    {
+        var builder = new StringBuilder();
+        foreach (var s in arr)
+        {
+            builder.Append(s).Append(", ");
+        }
+        return builder.ToString().Trim(' ').Trim(',');
+    }
+
+    //T10
+    public static int[] RemoveAt(int[] arr, int at)
+    {
+        return [.. arr.Where(x => x != at)];
+    }
+
+    //T11
+    public static int IndexOf(int[] arr, int value)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == value)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //T12
+    public static int[] SliceFromTail(int[] arr, int amount)
+    {
+        var newArr = new int[amount];
+        for (int i = 0, j = arr.Length - 1; i < newArr.Length; i++, j--)
+        {
+            newArr[i] = arr[j];
+        }
+
+        return [.. newArr.Reverse()];
+    }
+
+    //T13
+    public static void SwapMiddle(int[] arr, int newValue)
+    {
+        if (arr.Length % 2 == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == arr.Length / 2)
+            {
+                arr[i] = newValue;
+            }
+        }
+    }
+
+    //T14
+    public static int SumArray(int[] arr)
+    {
+        return arr.Aggregate((sum, i) => sum + i);
+    }
+
+    //T15
+    public static int Average(int[] arr)
+    {
+        return arr.Aggregate((sum, i) => sum + i) / arr.Length;
+    }
+
+    //T16
+    public static int[] MultiplyElementsNTimes(int[] arr, int n)
+    {
+        var intList = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                intList.Add(arr[j]);
+            }
+        }
+
+        return [.. intList];
+    }
+
+    //T17
+    public static int[] Unique(int[] arr)
+    {
+        return new HashSet<int>(arr).ToArray();
+    }
+
+    //T18
+    public static void SwapHeadAndTail(int[] arr)
+    {
+        (arr[^1], arr[0]) = (arr[0], arr[^1]);
     }
 
     //T19
@@ -61,5 +207,20 @@ public static class Taulukot
             }
             toConvert[i] = builder.ToString();
         }
+    }
+
+    //T20
+    public static int[] RemoveSmallerThan(int[] arr, int bound)
+    {
+        var smallerThanBound = new List<int>();
+
+        foreach (var item in arr)
+        {
+            if (item >= bound)
+            {
+                smallerThanBound.Add(item);
+            }
+        }
+        return [.. smallerThanBound];
     }
 }
